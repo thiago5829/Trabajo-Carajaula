75689f4d043d2bba7ce8d4f118c7212dc1fe9d3e
58097815 <58097815@10.36.117.100>
2026-08-19 15:11:52 -0300

    xd

--- Carajaula/src/principal/interfaz.java
+++ Carajaula/src/principal/interfaz.java
@@ -2,15 +2,25 @@ package principal;
 import javax.swing.*;
 public class interfaz {
 	JFrame ventana;
+	
 	public interfaz() {
 		ventana = new JFrame();
 	}
 	public void ventanaprincipal() {
-		ventana.setTitle("Ingresar");
+		ventana.setTitle("Gestion de figuritas");
 		ventana.setSize(750,500);
 		ventana.setLayout(null);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventana.setLocationRelativeTo(null);
 		ventana.setVisible(true);
+	JLabel titulo = new JLabel("Antes de empezar Registrate o Inicia Sesión");
+	titulo.setBounds(235,50,400,100);
+	ventana.add(titulo);
+	JButton iniciar_sesion = new JButton ("Iniciar Sesión");
+	iniciar_sesion.setBounds(375,250,150,25);
+	ventana.add(iniciar_sesion);
+	JButton registrarse = new JButton ("Registrarse");
+	registrarse.setBounds(215,250,125,25);
+	ventana.add(registrarse);
 	}
-}
+}	
