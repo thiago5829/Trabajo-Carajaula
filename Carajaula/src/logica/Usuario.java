d01e697e5756296f44ca915084769ce27c2380a2
58097815 <58097815@10.36.117.100>
2026-08-19 16:19:27 -0300

    19/8

--- Carajaula/src/logica/Usuario.java
+++ Carajaula/src/logica/Usuario.java
@@ -1,5 +1,23 @@
 package logica;
 
 public class Usuario {
-
+	private char nomusuarios;
+	private char contraseña;
+	public Usuario (char user, char contra) {
+		this.nomusuarios=user;
+		this.contraseña=contra;
+	}
+	public char getUsuarios() {
+		return nomusuarios;
+	}
+	public void setUsuarios(char usuarios) {
+		this.nomusuarios = usuarios;
+	}
+	public char getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(char contraseña) {
+		this.contraseña = contraseña;
+	}
+	
 }
---
+++ Carajaula/src/logica/usuarios.java
@@ -0,0 +1,5 @@
+package logica;
+import java.util.ArrayList;
+public class usuarios {
+}
+
