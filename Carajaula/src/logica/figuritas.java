05ed476f169f70b79e27f037651c709b594142eb
thiago5829 <thiangometro@gmail.com>
2026-09-06 18:19:25 -0300

    Add Usuario class for user management

--- Carajaula/src/logica/figuritas.java
+++ Carajaula/src/logica/figuritas.java
@@ -1,10 +1,23 @@
 package logica;
-import logica.figurita;
-public class figuritas {
-		figurita[] pegadas = new figurita[979];
 
-		
-}
-
+public class Usuario {
+	private String nomusuarios;
+	private String contraseña;
+	public Usuario (String user, String contra) {
+		this.nomusuarios=user;
+		this.contraseña=contra;
+	}
+	public String getUsuarios() {
+		return nomusuarios;
+	}
+	public void setUsuarios(String usuarios) {
+		this.nomusuarios = usuarios;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
 	
-
+}
