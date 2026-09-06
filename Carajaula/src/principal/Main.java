9835b728a1f41547f08695c50461635d4b6a1096
thiago5829 <thiangometro@gmail.com>
2026-09-06 18:23:40 -0300

    Add user registration and login options in Main

--- Carajaula/src/principal/Main.java
+++ Carajaula/src/principal/Main.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 import logica.Usuario;
 public class Main {
-
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
 		interfaz v1= new interfaz();
+		interfaz v2= new interfaz();
 		v1.ventanaprincipal();
+		v2.ventana2();
+		System.out.println("1-registrarse 2-Iniciar Sesion");
+		int numero = entrada.nextInt();
+		if (numero<2) {
+			System.out.println("elegiste registrarte");
+		}else {
+			System.out.println("elegiste iniciar sesion");
 		}
+	}
 	}
