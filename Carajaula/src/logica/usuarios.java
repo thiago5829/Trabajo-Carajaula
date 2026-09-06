a457c879195b16771807b14e28a8347a5c5de80c
thiago5829 <thiangometro@gmail.com>
2026-09-06 18:18:41 -0300

    Refactor usuarios class to manage Usuario objects
    Refactored 'usuarios' class to include 'agregar_usuario' method and an ArrayList of 'Usuario' objects.

--- Carajaula/src/logica/usuarios.java
+++ Carajaula/src/logica/usuarios.java
@@ -1,5 +1,9 @@
 package logica;
+import logica.Usuario;
 import java.util.ArrayList;
 public class usuarios {
+	ArrayList<Usuario> Usuarios = new ArrayList<Usuario>();
+	public void agregar_usuario(String usuario, String contra ) {
+	}
 }
 
