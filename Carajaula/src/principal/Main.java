88fbce9f7ccf00cb03625f92c02c5243fa5f5fb5
58097815 <58097815@192025000027564>
2026-09-11 15:43:25 -0300

    xddddd

--- Carajaula/src/principal/Main.java
+++ Carajaula/src/principal/Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 import logica.usuarios;
 public class Main {
 	public static void main(String[] args) {
-		usuarios listaUsuarios = new usuarios();
-		listaUsuarios.agregar_usuario("Thiago", "Admin123");
 		Scanner entrada = new Scanner(System.in);
 		interfaz v1= new interfaz();
 		v1.ventanaprincipal();
